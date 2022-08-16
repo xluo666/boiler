296b0f1ed1b7ee44bed5053462812488b0b6db0a
Xinyi Luo <xl459@njit.edu>
2022-08-15 21:36:22 -0400

    temp

---
+++ client/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <p>
+        hello
+      </p>
+    );
+  }
+}
+export default App;
